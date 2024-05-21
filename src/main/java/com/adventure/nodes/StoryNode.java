b30d7b3fb9db3cb383b2f25be20f0ddb765a0e60
alby <parralbi@gmail.com>
2024-05-21 12:46:35 +0200

    Add constructor for StoryNode that set TargetView

--- src/main/java/com/adventure/nodes/StoryNode.java
+++ src/main/java/com/adventure/nodes/StoryNode.java
@@ -2,9 +2,16 @@ package com.adventure.nodes;
 
 import com.adventure.interfaces.ApplicationContext;
 
+import java.util.Objects;
+
 public abstract class StoryNode
 {
     private String targetView;
+
+    public StoryNode( String targetView){
+        Objects.requireNonNull(targetView, "targetView can't be null");
+        this.targetView = targetView;
+    }
 
     public void setTargetView(String targetView)
     {
@@ -14,5 +21,6 @@ public abstract class StoryNode
     public String getTargetView() {
         return this.targetView;
     }
+
 
 }
