ad604363fbc485b1b22ab2fa382300c827a9e4f1
Alessio Checchin <alessiochecchin@gmail.com>
2024-05-26 16:55:10 +0200

    Fix unix paths Resources

--- src/main/java/com/adventure/Resources.java
+++ src/main/java/com/adventure/Resources.java
@@ -11,28 +11,21 @@ public class Resources
 {
     public static BackgroundImage getBackground(String source)
     {
-        try
+        URL imageUrl = Resources.class.getResource(source);
+
+        if (imageUrl != null)
         {
-            URL imageUrl = Resources.class.getResource(source);
-
-            if (imageUrl != null)
-            {
-                String filePath = String.valueOf(Paths.get(imageUrl.toURI()).toFile());
-
-                return new BackgroundImage(
-                        new Image(filePath),
-                        BackgroundRepeat.NO_REPEAT,
-                        BackgroundRepeat.NO_REPEAT,
-                        BackgroundPosition.DEFAULT,
-                        // Set to cover
-                        new BackgroundSize(0,0, false, false, false, true)
-                );
-            }
+            return new BackgroundImage(
+                    new Image(imageUrl.toString()),
+                    BackgroundRepeat.NO_REPEAT,
+                    BackgroundRepeat.NO_REPEAT,
+                    BackgroundPosition.DEFAULT,
+                    // Set to cover
+                    new BackgroundSize(0,0, false, false, false, true)
+            );
         }
-        catch(URISyntaxException ignored) {}
 
         return null;
-
     }
 
 }
