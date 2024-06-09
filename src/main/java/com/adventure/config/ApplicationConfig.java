b078769003fdb43db05794e9de4d6d770acde4f0
Jacopo <jacopo@studiotoniolo.eu>
2024-06-09 15:07:20 +0200

    New save folder management system New StorageService management system

--- src/main/java/com/adventure/config/ApplicationConfig.java
+++ src/main/java/com/adventure/config/ApplicationConfig.java
@@ -7,6 +7,9 @@ import com.adventure.services.StorageService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class ApplicationConfig implements Config
@@ -19,17 +22,35 @@ public class ApplicationConfig implements Config
             this.displayWidth = Integer.parseInt(properties.getProperty("display.width"));
             this.displayHeight = Integer.parseInt(properties.getProperty("display.height"));
             this.resizable = Boolean.parseBoolean(properties.getProperty("resizable"));
+            this.configFolder = properties.getProperty("config.folder");
+
+            // Ensure existence of the config folder
+            if(!new File(configFolder).exists())
+                new File(configFolder).mkdirs();
 
             // Default storage provider is filesystem.
-            String storageProvider = properties.getProperty("storage.provider", "filesystem");
+            String storageProvider = "filesystem";
 
+            // Try to obtain keys from key.conf
+            try  (InputStream keys = new FileInputStream(new File(properties.getProperty("config.folder") + "key.conf")))
+            {
+                Properties keyProps = new Properties();
+                keyProps.load(keys);
+                properties.setProperty("storage.aws.key",keyProps.getProperty("storage.aws.key"));
+                properties.setProperty("storage.aws.secret",keyProps.getProperty("storage.aws.secret"));
+                properties.setProperty("storage.aws.bucket.name",keyProps.getProperty("storage.aws.bucket.name"));
+                properties.setProperty("storage.aws.region",keyProps.getProperty("storage.aws.region"));
+                storageProvider = "aws";
+            } catch (Exception ex) { /*No aws key file, just local game*/ }
+
+            // If key.conf exists and has keys, BucketStorage is used
             if(storageProvider.equals(SupportedStorage.AWS.name().toLowerCase()))
             {
                 this.storageService = new BucketStorageService(this);
             }
             else
             {
-                // Fallback storage service.
+                // Fallback to FileSystemStorage
                 logger.debug("Preferred storage provider set to filesystem...");
                 this.storageService = new FileSystemStorageService(this);
             }
@@ -44,6 +65,12 @@ public class ApplicationConfig implements Config
     public int getDisplayWidth()
     {
         return this.displayWidth;
+    }
+
+    @Override
+    public String getConfigFolder()
+    {
+        return this.configFolder;
     }
 
     @Override
@@ -99,6 +126,8 @@ public class ApplicationConfig implements Config
     private final StorageService storageService;
 
     private final Properties properties;
+
+    private final String configFolder;
 
     private static final Logger logger = LogManager.getLogger();
 }
